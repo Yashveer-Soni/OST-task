ef7e367a3ee7e311cb4af585898e97fdcb7cd231
Yashveer-Soni <officialyashveersoni@gmail.com>
2024-04-24 22:31:01 +0530

    sfsd

--- OST/main/views.py
+++ OST/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    if request.method=="POST":
+        files=request.FILES.get('files')
+        print(files)
+    return render(request, 'main/index.html')
